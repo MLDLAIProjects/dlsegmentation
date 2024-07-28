5e8f27118e607e9562077492e61d52ecd9a172ae
Sahil Verma <sahilverma@192.168.1.3>
2024-07-28 18:47:47 +0530

    project setup and requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'dlSegmentation',
+    version= '0.0.0',
+    author= 'Sahil Verma',
+    author_email= 'connect.sahil5@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
